09dd5d1b6d16577532d6c2235338a4ba982d5484
curLeeBrace <leeandreimain@gmail.com>
2023-10-22 01:17:47 +0800

    add function

--- client/app2.js
+++ client/app2.js
@@ -1,1 +1,5 @@
-console.log("made by branchA");
+console.log("made by branchA");//1
+console.log("made by branchA");//2
+console.log("made by branchA");//3
+console.log("made by branchA");//4
+console.log("made by branchA");//5
